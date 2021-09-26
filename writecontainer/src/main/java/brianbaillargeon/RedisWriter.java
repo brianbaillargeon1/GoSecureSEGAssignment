3a4657564cead6db7ab2eb7b6ea81fd1905397cf
Brian Baillargeon <brianbaillargeon@gmail.com>
2021-09-26 18:45:57 -0400

    Initial project set up for the java based container that will write to Redis

---
+++ writecontainer/src/main/java/brianbaillargeon/RedisWriter.java
@@ -0,0 +1,13 @@
+package brianbaillargeon;
+
+/**
+ * Hello world!
+ *
+ */
+public class RedisWriter 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ writecontainer/src/test/java/brianbaillargeon/RedisWriterTest.java
@@ -0,0 +1,20 @@
+package brianbaillargeon;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RedisWriterTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
